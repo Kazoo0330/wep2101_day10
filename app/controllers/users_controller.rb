f7e4c3fe779b05e9e63e1807c786010648f8665c
sachio <sachio.engineer@gmail.com>
2021-05-30 16:01:23 +0900

    issues#2 User登録機能完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to users_path
+      redirect_to user_path(@user.id)
+      flash[:success] = "新たなユーザ「#{@user.name}」が登録されました"
     else
       render :new
     end
@@ -21,11 +22,15 @@ class UsersController < ApplicationController
   def update
     if @user.update(user_params)
       redirect_to user_path(@user.id)
+      flash[:success] = "ユーザ「#{@user.name}」の編集が完了しました"
     else
       render :edit
     end
   end
   def destroy
+    @user.destroy
+    redirect_to new_user_path
+    flash[:danger] = "ユーザ「#{@user.name}」を削除しました"
   end
   private
   def set_user
